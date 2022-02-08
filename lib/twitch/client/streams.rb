2ac13d03d434eed5831cc16bb28528326cbb3f25
Kevin Milburn <kevin.milburn@lifejak.com>
2022-02-08 17:02:29 +0000

    Stream Metadata deprecated and removed... https://discuss.dev.twitch.tv/t/deprecation-of-the-helix-get-streams-metadata-endpoint/26407

--- lib/twitch/client/streams.rb
+++ lib/twitch/client/streams.rb
@@ -19,11 +19,6 @@ module Twitch
       def get_streams(options = {})
         initialize_response Stream, get('streams', options)
       end
-
-      ## TODO: Can't find this method in documentation, test it
-      def get_streams_metadata(options = {})
-        initialize_response StreamMetadata, get('streams/metadata', options)
-      end
     end
   end
 end
